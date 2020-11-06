0912d3a003af39e0785ca7de88d8c6fb1444d014
Toidi93 <73305038+Toidi93@users.noreply.github.com>
2020-11-06 17:23:48 +0100

    updated code
    en masse

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
+// opt is a map of the game
+// true = available
+// false = taken
+let opt = new Map();
+let counter = 1;
+const COUNTER_TEXT = "Potential sips: ";
+
+// start button
+function startgame() {
+    for (let i = 1; i <= 10; i++) {
+        opt.set(i, true);
+    }
+    document.getElementById("counter").innerText = COUNTER_TEXT + counter;
+
+    const num = getValidNumber();
+    document.getElementById("display").innerText = num;
+}
+
 // Func giv et random tal
-// Func tjekker om det er udtaget
+function randomNumber() {
+    let key = Math.random() * (opt.size - 1) + 1;
+    return Math.round(key);
+}
+
+// Gets a key and checks if it is true, then sets it to false
+function getValidNumber() {
+    let key = randomNumber();
+    while (!opt.get(key)) {
+        key = randomNumber();
+    }
+    opt.set(key, false);
+    console.log(opt)
+    return key;
+}
 
 // Func knap til over
+function higher() {
+    const nextnumber = getValidNumber()
+    const currentnumber = parseInt(document.getElementById("display").innerText);
+    if (currentnumber < nextnumber) {
+        counter++;
+    } else {
+        alert("DRINK " + counter);
+        counter = 1;
+    }
+    document.getElementById("counter").innerText = COUNTER_TEXT + counter;
+    document.getElementById("display").innerText = nextnumber;
+    console.log(nextnumber);
+}
+
 // Func knap til under
+function lower() {
+    const nextnumber = getValidNumber()
+    const currentnumber = parseInt(document.getElementById("display").innerText);
+    if (currentnumber > nextnumber) {
+        counter++;
+    } else {
+        alert("DRINK " + counter);
+        counter = 1;
+    }
+    document.getElementById("counter").innerText = COUNTER_TEXT + counter;
+    document.getElementById("display").innerText = nextnumber;
+    console.log(nextnumber);
+}
 
 // Func knap restart game (tømmer listen)
